7362a0ef8e968d8b6295a0e10a9af040333bf228
isagarcia11 <isabella.garciag@uqvirtual.edu.co>
2025-08-30 00:08:17 -0500

    Upload beta version of products management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { LayoutComponent } from "./layout/layout.component";
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, LayoutComponent],
+  imports: [RouterOutlet, LoginComponent, LayoutComponent, ProductsComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ProductsComponent } from './products/products.component';
 
 export const routes: Routes = [
-     { path: 'login', component: LoginComponent }
+     { path: 'login', component: LoginComponent },
+     {path: 'dashboard', component: LayoutComponent},
+     { path: 'inventario/productos', component: ProductsComponent },
+
 ];
