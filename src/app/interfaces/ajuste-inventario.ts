d946d61a183fdcb8bd8acccdfd8e09f352c0aea4
isagarcia11 <isabella.garciag@uqvirtual.edu.co>
2025-09-06 00:04:29 -0500

    Create new services, interfaces and components

---
+++ src/app/interfaces/ajuste-inventario.ts
@@ -0,0 +1,6 @@
+export interface AjusteInventario {
+    stock_item_id: number;
+    cantidad_contada: number;
+    motivo: string;
+    notas?: string; 
+}
---
+++ src/services/ajuste-inventario.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AjusteInventario } from '../app/interfaces/ajuste-inventario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AjusteInventarioService {
+  private apiUrl = 'https://tu-api.com/bodega/inventory/adjust/';
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Envía los datos de ajuste a la API
+   * @param ajuste El objeto con la información del conteo físico.
+   * @returns Un Observable con la respuesta de la API.
+   */
+  realizarAjuste(ajuste: AjusteInventario): Observable<any> {
+    return this.http.post(this.apiUrl, ajuste);
+  }
+}
